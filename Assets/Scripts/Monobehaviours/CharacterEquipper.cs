fd9131e283d7d9ecd267c7da36c5482db77c3197
Stroopwafel <sanderwilgeenspam+whttw@pm.me>
2025-08-16 13:27:49 +0200

    change cloths working!

--- Assets/Scripts/Monobehaviours/CharacterEquipper.cs
+++ Assets/Scripts/Monobehaviours/CharacterEquipper.cs
@@ -1,7 +1,38 @@
-using Opsive.UltimateInventorySystem.Demo.CharacterControl.Equiping;
+using AdvancedPeopleSystem;
+using Opsive.UltimateInventorySystem.Core.DataStructures;
+using Opsive.UltimateInventorySystem.Equipping;
+using UnityEngine;
 
-public class CharacterEquipper : DemoCharacterEquipper {
+public class CharacterEquipper : EquipperBase {
+
+    [SerializeField]
+    [Tooltip("Required -> This is the Advanced People System reference coming from the visuals character.")]
+    private CharacterCustomization character;
+
     protected override void Awake() {
         base.Awake();
+    }
+
+    protected override void OnAddedItemToInventory(ItemInfo originItemInfo, ItemStack addedItemStack) {
+        if (addedItemStack == null) { return; }
+        if (addedItemStack.ItemCollection == m_EquipmentItemCollection) {
+            var index = m_EquipmentItemCollection.GetItemSlotIndex(addedItemStack);
+            Debug.Log("I Equip!");
+            Equip(addedItemStack.Item, index);
+            ChangeCloths();
+        }
+    }
+
+    /// <summary>
+    /// Calls the CharacterCustomization API to change the cloths.
+    /// TODO: add fields for the type and index, then make it possible to swap the old cloths to drop
+    /// </summary>
+    private void ChangeCloths() {
+        if (character == null) {
+            Debug.LogError("You didnt drag the reference to CharacterCustomization yo...");
+            return;
+        }
+        var lastShirtIndex = character.Settings.shirtsPresets.Count - 1;
+        character.SetElementByIndex(CharacterElementType.Shirt, 8); //Set shirt element by Id
     }
 }
